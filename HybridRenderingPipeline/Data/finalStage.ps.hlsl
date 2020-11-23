a2abe7eab785b08b351cf3313f9abb62a8cd82ef
Zijing <zjpeng789@outlook.com>
2020-11-23 11:18:31 -0500

    reorganize files

---
+++ HybridRenderingPipeline/Data/finalStage.ps.hlsl
@@ -0,0 +1,42 @@
+/**********************************************************************************************************************
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+# following conditions are met:
+#  * Redistributions of code must retain the copyright notice, this list of conditions and the following disclaimer.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT
+# SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************************************/
+// Some shared Falcor stuff for talking between CPU and GPU code
+#include "HostDeviceSharedMacros.h"
+#include "HostDeviceData.h"      
+
+
+Texture2D<float4>   gAO;           
+Texture2D<float4>   gShadow;  
+Texture2D<float4>   gReflection; 
+Texture2D<float4>   gDirectLighting;
+
+float4 main(float2 texC : TEXCOORD, float4 pos : SV_Position) : SV_Target0
+{
+    uint2 pixelPos = (uint2)pos.xy;
+    float4 shadow = gShadow[pixelPos];
+    float4 reflection = gReflection[pixelPos];
+	float4 directLighting = gDirectLighting[pixelPos];
+    float4 ambientOcclusion = gAO[pixelPos];
+
+	float3 shadeColor;
+
+	// Todo: reflection
+	shadeColor = (directLighting * ambientOcclusion * shadow).rgb;
+
+	return float4(shadeColor, 1.0f);
+}
---
+++ HybridRenderingPipeline/HybridRendering.cpp
@@ -0,0 +1,54 @@
+/**********************************************************************************************************************
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+# following conditions are met:
+#  * Redistributions of code must retain the copyright notice, this list of conditions and the following disclaimer.
+#  * Neither the name of NVIDIA CORPORATION nor the names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT
+# SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**********************************************************************************************************************/
+
+#include "Falcor.h"
+#include "../SharedUtils/RenderingPipeline.h"
+#include "../CommonPasses/SimpleGBufferPass.h"
+#include "Passes/AmbientOcclusionPass.h"
+#include "Passes/ShadowPass.h"
+#include "Passes/ReflectionPass.h"
+#include "../CommonPasses/CopyToOutputPass.h"
+#include "Passes/DirectLightingPass.h"
+#include "Passes/FinalStagePass.h"
+#include "../CommonPasses/SimpleAccumulationPass.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	// Create our rendering pipeline
+	RenderingPipeline *pipeline = new RenderingPipeline();
+
+	// Add passes into our pipeline
+	pipeline->setPass(0, SimpleGBufferPass::create());        
+	pipeline->setPass(1, AmbientOcclusionPass::create("aoChannel"));
+	pipeline->setPass(2, SimpleAccumulationPass::create("aoChannel"));
+	pipeline->setPass(3, ShadowPass::create("shadowChannel"));
+	pipeline->setPass(4, SimpleAccumulationPass::create("shadowChannel"));
+	pipeline->setPass(5, ReflectionPass::create("reflectionChannel"));
+	pipeline->setPass(6, SimpleAccumulationPass::create("reflectionChannel"));
+	pipeline->setPass(7, DirectLightingPass::create("directLightingChannel"));
+	pipeline->setPass(8, FinalStagePass::create());
+	//pipeline->setPass(9, SimpleAccumulationPass::create());
+
+	// Define a set of config / window parameters for our program
+    SampleConfig config;
+    config.windowDesc.title = "Hybrid Rendering";
+    config.windowDesc.resizableWindow = true;
+
+	// Start our program!
+	RenderingPipeline::run(pipeline, config);
+}

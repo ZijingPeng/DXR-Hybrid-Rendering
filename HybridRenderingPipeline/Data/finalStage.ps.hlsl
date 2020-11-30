59391f252c4722fb21af8b4a539830b7e0bcfbb0
AsteriskChan <1245577954@qq.com>
2020-11-30 15:14:30 +0000

    multiply

--- HybridRenderingPipeline/Data/finalStage.ps.hlsl
+++ HybridRenderingPipeline/Data/finalStage.ps.hlsl
@@ -37,7 +37,7 @@ float4 main(float2 texC : TEXCOORD, float4 pos : SV_Position) : SV_Target0
 
 	// Todo: reflection
 	//shadeColor = (directLighting * ambientOcclusion * shadow).rgb + reflection.rgb;
-	//shadeColor = (directLighting * ambientOcclusion * shadow * reflection).rgb;
-	shadeColor = reflection.rgb;
+	shadeColor = (directLighting * ambientOcclusion * shadow * reflection).rgb;
+	//shadeColor = reflection.rgb;
 	return float4(shadeColor, 1.0f);
 }

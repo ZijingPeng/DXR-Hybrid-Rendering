cff5c319bdb338413047c79d14b1f10eba85529a
AsteriskChan <1245577954@qq.com>
2020-12-07 02:46:01 +0000

    Fix errors

--- HybridRenderingPipeline/Data/merge.ps.hlsl
+++ HybridRenderingPipeline/Data/merge.ps.hlsl
@@ -7,6 +7,6 @@ Texture2D<float4>   gM2;
 float4 main(float2 texC : TEXCOORD, float4 pos : SV_Position) : SV_Target0
 {
   uint2 pixelPos = (uint2)pos.xy;
-	float3 shadeColor = gM1[pixesPos].rgb * gM2[pixelPos].rgb;
+	float3 shadeColor = gM1[pixelPos].rgb * gM2[pixelPos].rgb;
 	return float4(shadeColor, 1.0f);
 }
